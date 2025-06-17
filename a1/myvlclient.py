7e93fc0483cd9bfc08edb777a0acc2aa19aca19b
Robert Chien <robertchien4948@gmail.com>
2025-06-17 14:08:34 -0700

    Code finished, readme TODO

--- a1/myvlclient.py
+++ a1/myvlclient.py
@@ -11,10 +11,17 @@ clientSocket.connect((serverName, serverPort))
 
 message = input('Input sentence (in lowercase): ')
 msg_len = len(message)
+#attach length indicator to front of sentence
+if msg_len < 10: #padding length indicator as needed
+    msg_len = "0"+str(msg_len)
+else: msg_len = str(msg_len)
 #assemble into full message sent:
-msg_full = str(msg_len) + message
-print("DEBUG: ", msg_full)
+msg_full = msg_len + message
+# print("DEBUG: ", msg_full)
+
+#Send message
 clientSocket.send(msg_full.encode())
 
+#Receive new message
 mod_message = clientSocket.recv(64)
-print("Server says: " + mod_message.decode())
+print("From server: " + mod_message.decode())
--- a1/myvlserver.py
+++ a1/myvlserver.py
@@ -1,21 +1,28 @@
 from socket import *
-
 serverPort = 12000
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-
 serverSocket.bind(('', serverPort))
-print("Server now running on port ", serverPort, ". \n")
-while True:
-    serverSocket.listen(1)
+print("Server now running on port", serverPort, ".")
 
-    conn_Socket, addr = serverSocket.accept()
-    #accept client connection
-    print("CONNECTED: ", str(addr))
+serverSocket.listen(1)
+conn_Socket, addr = serverSocket.accept()
+#accept client connection, show client IP
+print("CONNECTED: ", str(addr[0]))
 
-    sentence = conn_Socket.recv(64).decode()
-    new_sentence = sentence.upper()
-    #uppercase sentence
+sentence = conn_Socket.recv(64).decode()
+#split payload into length and sentence
+sentence_len = int(sentence[0] + sentence[1])
+print("msg_length: ", sentence_len)
+sentence_content = sentence[2:]
+print("processed: ", sentence_content)
 
-    conn_Socket.send(new_sentence.encode())
-    conn_Socket.close()
+new_sentence = sentence_content.upper()
+#uppercase sentence
+new_len = len(new_sentence)
+#track length for print output...
+
+conn_Socket.send(new_sentence.encode())
+print("msg_len_sent: ", new_len)
+conn_Socket.close()
+print("Connection Closed.")
