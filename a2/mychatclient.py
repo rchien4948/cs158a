525990cb2022c98c5cc63c9d952f1b9feee6b11a
Robert Chien <robertchien4948@gmail.com>
2025-06-28 14:10:37 -0700

    Basic setups

--- a2/mychatclient.py
+++ a2/mychatclient.py
@@ -3,3 +3,12 @@ from socket import *
 serverName = input('Enter Server Name: ')
 serverPort = input('Enter Server Port: ')
 
+#TCP Socket Specs
+clientSocket = socket(AF_INET, SOCK_STREAM)
+#Connecting
+clientSocket.connect((serverName, serverPort))
+
+
+chat_message = input('Input sentence (in lowercase): ')
+#Send message
+clientSocket.send(chat_message.encode())
--- a2/mychatserver.py
+++ a2/mychatserver.py
@@ -1,3 +1,11 @@
 from socket import *
+import threading
+import time
 
 serverPort = 12000
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+print("Server now running on port", serverPort, ".")
+
+threads = []
